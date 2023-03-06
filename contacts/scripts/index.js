f21edf722fb1624d40ddc55dc3cb93b0c04099c8
Nikita Kulichev <longrast.2002@gmail.com>
2023-03-06 19:04:02 +0300

    add ymaps

--- contacts/scripts/index.js
+++ contacts/scripts/index.js
@@ -1,27 +1,4 @@
-/*main();
-
-  async function main() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда
-    // загрузятся все компоненты API.
-    await ymaps3.ready;
-    // Создание карты.
-    const map = new ymaps3.YMap(document.getElementById('map-container"'), {
-      location {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «долгота, широта».
-        center: [37.64, 55.76],
-
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 7
-      },
-      [
-        // Добавляем слой для отображения схематической карты Яндекса.
-        new ymaps3.YMapDefaultSchemeLayer();
-      ]
-    })
-  }*/
-  var prevScrollpos = window.pageYOffset;
+var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
@@ -31,3 +8,35 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+ymaps.ready(init);
+
+function init(){
+  var map = new ymaps.Map('map-container', {
+    center: [55.65, 37.43],
+    zoom: 12,
+    controls: ['zoomControl'],
+    behaviours: ['drag']
+  });
+  var placemark1 = new ymaps.Placemark([55.65, 37.43], {
+    hintContent: '<div class="map__hint1">55.65, 37.43</div>',
+    balloonContent: 'KENT1'
+  });
+  var placemark2 = new ymaps.Placemark([55.67, 37.45], {
+    hintContent: '<div class="map__hint2">56.65, 38.43</div>',
+    balloonContent: 'KENT2'
+  });
+  map.geoObjects.add(placemark1);
+  map.geoObjects.add(placemark2);
+  console.log("работает");
+}
+/*
+ymaps3.ready.then(() => {
+  // HTML-элемент.
+  const moscow_map = new ymaps3.YMap(document.getElementById('#map-container'), {
+    location: {
+      center: [37.64, 55.76],
+      zoom: 10
+    }
+  });
+});*/
